af335f393276ef022a98359a504dbbd0e919af95
zhangxin <zhangxin@smarchit.com>
2019-05-09 15:51:48 +0800

    init save

---
+++ .IntelliJIdea2019.1/system/tmp/intellij-ssh-askpass.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"E:/devt/WebStorm 2019.1/jre64/bin/java" -cp "E:/devt/WebStorm 2019.1/plugins/git4idea/lib/git4idea-rt.jar;E:/devt/WebStorm 2019.1/lib/xmlrpc-2.0.1.jar;E:/devt/WebStorm 2019.1/lib/commons-codec-1.10.jar;E:/devt/WebStorm 2019.1/lib/util.jar" org.jetbrains.git4idea.nativessh.GitNativeSshAskPassApp "$@"
